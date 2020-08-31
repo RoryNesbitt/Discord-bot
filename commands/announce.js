5f0cd714bea6e3c381a29b15ebe5a2ed3d4fd8df
rory <hamster2020@live.com>
2020-08-31 02:07:39 +0100

    Added in ability to get prefix

--- commands/announce.js
+++ commands/announce.js
@@ -3,7 +3,12 @@ module.exports = {
 	description: `The bit that announces`,
 	usage: `announce <What you want me to say>`,
 	execute(message) {
-
+		const config = require(`../config.json`);
+        const id = message.guild.id;
+        let prefix;
+        config.servers.forEach(server => {
+            if (server.id == id) prefix = server.prefix;
+        });
 		if (message.member.hasPermission(`ADMINISTRATOR`) || message.member.roles.cache.has(`691665461586427944`)) {
 			message.delete();
 			message.channel.send(message.content.slice(prefix.length + 8));
