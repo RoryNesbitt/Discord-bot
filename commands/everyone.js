8a39325386e0c2442542ba0b117a775f7b2d0895
rory <hamster2020@live.com>
2020-10-10 03:35:55 +0100

    Added random replies

--- commands/everyone.js
+++ commands/everyone.js
@@ -15,12 +15,38 @@ module.exports = {
                }
           });
 
+          let num;
+          let reply;
           switch (id) {
                case `709166827254841538`: //DFZ
-                    message.reply("Gonnie naw!")
+                    num = 3;
+                    reply = Math.ceil(Math.random() * num);
+                    switch (reply) {
+                         case 1:
+                              message.reply(`gonnie naw!`);
+                              break;
+                         case 2:
+                              message.channel.send(`https://tenor.com/view/idiot-gordon-ramsay-fucking-donkey-gif-4653512`);
+                              break;
+                         case 3:
+                              message.reply("here you, you wank!");
+                              break;
+                    }
                     break;
                case `718251835969896448`: //OK
-                    message.reply("Here you, you wank!")
+                    num = 3;
+                    reply = Math.ceil(Math.random() * num);
+                    switch (reply) {
+                         case 1:
+                              message.reply(`gonnie naw!`);
+                              break;
+                         case 2:
+                              message.channel.send(`https://tenor.com/view/idiot-gordon-ramsay-fucking-donkey-gif-4653512`);
+                              break;
+                         case 3:
+                              message.reply("here you, you wank!");
+                              break;
+                    }
                     break;
           }
      },
